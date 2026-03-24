a6c29e11d68dbc1172e91c36c29803396befa17c
Mattia Soldani <mattiasoldani93@gmail.com>
2026-03-24 18:13:31 +0100

    created folder for online scripts, script for live sync

---
+++ online/syncall.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SRCPATH="/home/daq/CAEN_Janus_5202_Win"
+DSTPATH="msoldani@lxplus.cern.ch:/eos/experiment/newtile/beamtests/26_05_t10/fers_daq"
+SFSPATH="/home/daq/eos_temp_fers"
+
+echo "Mounting $SFSPATH into $DSTPATH (may fail if already mounted)"
+sshfs $DSTPATH $SFSPATH
+echo "---"
+
+echo "Starting live sync between local:"
+echo "$SRCPATH"
+echo "and remote (is it mounted?):"
+echo "$SFSPATH (--> $DSTPATH)"
+echo "Kill the process to interrupt"
+echo "---"
+
+i=0
+while true
+do
+
+trap "exit" SIGINT
+
+echo "Iteration number $i..."
+i=$(($i + 1))
+
+rsync -avz $SRCPATH/* $SFSPATH/. > /dev/null
+
+echo "---"
+
+echo "Done"
+
+echo "---"
+
+sleep 1
+
+done
